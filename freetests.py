ac8ccf1b4364a58c16e0f2f2ad1bc9fcafd20374
Abram Hindle <abram.hindle@softwareprocess.es>
2014-01-02 20:42:37 -0700

    Assignment 4: AJAX

---
+++ freetests.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# Copyright 2013 Abram Hindle
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# run python freetests.py
+
+import urllib2
+import unittest
+import urlparse
+import json
+# your server
+import server
+import random
+
+BASEHOST = '127.0.0.1'
+BASEPORT = 5000
+
+class ServerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        '''Check this out: we're not actually doing HTTP we're just calling the webservice directly'''
+        self.app = server.app.test_client()
+
+    def tearDown(self):
+        '''nothing'''
+        
+    def testNothing(self):
+        '''nothing'''
+
+    def testHello(self):
+        r = self.app.get('/')
+        # a redirect is ok!
+        self.assertTrue(r.status_code == 200 or
+                        r.status_code == 301 or
+                        r.status_code == 302
+                        , "Code not 200!")
+        if (r.status_code == 200):
+            self.assertTrue(len(r.data) > 5, "No data?")
+
+    def testUpdate(self):
+        v = 'T'+str(random.randint(1,1000000))
+        r = self.app.get(('/entity/%s' % v))
+        self.assertTrue(r.status_code == 200, "Code not 200!")
+        self.assertTrue(r.data == '{}', "Not empty? %s" % r.data)
+        d = {'x':2, 'y':3}
+        r = self.app.put(('/entity/%s' % v),data=json.dumps(d))
+        self.assertTrue(r.status_code == 200, "PUT Code not 200!")
+        rd = json.loads(r.data)
+        for key in d:
+            self.assertTrue(rd[key] == d[key], "KEY %s " % key)
+        r = self.app.get(('/entity/%s' % v))
+        self.assertTrue(r.status_code == 200, "Code not 200!")
+        self.assertTrue(json.loads(r.data) == d, "D != r.data")
+
+        
+    def populateWorld(self):
+        self.world = dict()
+        for i in range(1,20):
+            v = 'P'+str(random.randint(1,1000000))
+            x = random.randint(1,640)
+            y = random.randint(1,480)
+            c = random.choice(['red','green','blue'])
+            self.world[v] = {'x':x,'y':y,'colour':c}
+        return self.world
+
+    def testWorld(self):
+        self.populateWorld()
+        r = self.app.post('/clear')
+        self.assertTrue(r.status_code == 200, "Code not 200!")
+        for key in self.world:
+            r = self.app.put(('/entity/%s' % key),
+                             data=json.dumps(self.world[key]))
+            self.assertTrue(r.status_code == 200, "Code not 200!")
+            j = json.loads(r.data)
+            self.assertTrue(len(j.keys()) >= 3,"JSON lacking keys! %s" % j.keys())
+        r = self.app.get('/world')
+        self.assertTrue(r.status_code == 200, "Code not 200!")
+        newworld = json.loads(r.data)
+        for key in self.world:
+            self.assertTrue(self.world[key]  == newworld[key], "Key %s" % key)
+
+
+        
+        
+        
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ server.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# Copyright 2013 Abram Hindle
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# You can start this by executing it in python:
+# python server.py
+#
+# remember to:
+#     pip install flask
+
+
+import flask
+from flask import Flask, request
+import json
+app = Flask(__name__)
+app.debug = True
+
+# An example world
+# {
+#    'a':{'x':1, 'y':2},
+#    'b':{'x':2, 'y':3}
+# }
+
+class World:
+    def __init__(self):
+        self.clear()
+        
+    def update(self, entity, key, value):
+        entry = self.space.get(entity,dict())
+        entry[key] = value
+        self.space[entity] = entry
+
+    def set(self, entity, data):
+        self.space[entity] = data
+
+    def clear(self):
+        self.space = dict()
+
+    def get(self, entity):
+        return self.space.get(entity,dict())
+    
+    def world(self):
+        return self.space
+
+# you can test your webservice from the commandline
+# curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
+
+myWorld = World()          
+
+# I give this to you, this is how you get the raw body/data portion of a post in flask
+# this should come with flask but whatever, it's not my project.
+def flask_post_json():
+    '''Ah the joys of frameworks! They do so much work for you
+       that they get in the way of sane operation!'''
+    if (request.json != None):
+        return request.json
+    elif (request.data != None and request.data != ''):
+        return json.loads(request.data)
+    else:
+        return json.loads(request.form.keys()[0])
+
+@app.route("/")
+def hello():
+    '''Return something coherent here.. perhaps redirect to /static/index.html '''
+    return None
+
+@app.route("/entity/<entity>", methods=['POST','PUT'])
+def update(entity):
+    '''update the entities via this interface'''
+    return None
+
+@app.route("/world", methods=['POST','GET'])    
+def world():
+    '''you should probably return the world here'''
+    return None
+
+@app.route("/entity/<entity>")    
+def get_entity(entity):
+    '''This is the GET version of the entity interface, return a representation of the entity'''
+    return None
+
+@app.route("/clear", methods=['POST','GET'])
+def clear():
+    '''Clear the world out!'''
+    return None
+
+if __name__ == "__main__":
+    app.run()

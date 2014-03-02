c35e52eab9063f718eb58e443ea5c229ec3bda1c
geominion <geojbd@gmail.com>
2014-03-02 15:46:21 -0700

    I pass the first 2 useless tests

--- server.py
+++ server.py
@@ -22,7 +22,7 @@
 
 
 import flask
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 import json
 app = Flask(__name__)
 app.debug = True
@@ -73,29 +73,26 @@ def flask_post_json():
 
 @app.route("/")
 def hello():
-    name=None
-    '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return render_template('index.html', name=name)
+    return app.send_static_file('index.html')
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
-def update(entity):
-    '''update the entities via this interface'''
-    return None
+def update(entity): 
+    return entity
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return jsonify(myWorld.get())
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
-    '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
